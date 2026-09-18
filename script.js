0b7450d301baf84dc5195a2b52f111b9245cb604
Vishnu <vikkyvishnu34@gmail.com>
2026-09-18 00:39:39 +0000

    Normalize live Vercel asset URLs

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.querySelectorAll('img[src^="/assets/"], [data-img^="/assets/"]').forEach(element => {
+  const attribute = element.hasAttribute('src') ? 'src' : 'data-img';
+  element.setAttribute(attribute, element.getAttribute(attribute).replace('/assets/', '/'));
+});
+
 const $ = (s, r=document) => r.querySelector(s);
 const $$ = (s, r=document) => [...r.querySelectorAll(s)];
 
